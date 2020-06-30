3e51631e8b4b9595a8a469cc6bb76515922ae26d
Venessa <v_campbell10@yahoo.com>
2020-06-30 14:43:18 -0400

    fix: removed exposed secret

--- server/index.js
+++ server/index.js
@@ -4,16 +4,13 @@ const morgan = require('morgan')
 const compression = require('compression')
 const session = require('express-session')
 const passport = require('passport')
-//const SequelizeStore = require('connect-session-sequelize')(session.Store)
-//const db = require('./db')
-//const sessionStore = new SequelizeStore({db})
 const PORT = 8080
 const mongoose = require('mongoose')
 const socketio = require('socket.io')
 const graphqlHttp = require('express-graphql')
 const app = express()
 const schema = require('./db/models/schema')
-//const cors = require('cors')
+
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -31,18 +28,6 @@ module.exports = app
  * Node process on process.env
  */
 if (process.env.NODE_ENV !== 'production') require('../secrets')
-
-// passport registration
-// passport.serializeUser((user, done) => done(null, user.id))
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await db.models.user.findByPk(id)
-//     done(null, user)
-//   } catch (err) {
-//     done(err)
-//   }
-// })
 
 const createApp = () => {
   // logging middleware
@@ -77,10 +62,7 @@ const createApp = () => {
   //where do you find the schemas
   //connect mongodb
   mongoose
-    .connect(
-      'mongodb+srv://Venessa:w87ny781@cluster0-lkxcw.mongodb.net/Conversate?retryWrites=true&w=majority',
-      {useNewUrlParser: true, useUnifiedTopology: true}
-    )
+    .connect((mongodb + srv: {useNewUrlParser: true, useUnifiedTopology: true}))
     .then(() => {
       console.log('mongoose connected')
     })
